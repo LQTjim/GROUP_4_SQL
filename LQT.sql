e4712ae7a574f4fbe3270836353edbd276a396fd
LQTjim <jim6050482@hotmail.com>
2023-05-11 16:03:57 +0800

    add:LQT.sql member table join other table

---
+++ LQT.sql
@@ -0,0 +1,21 @@
+--- 會員查表 join sex level role
+SELECT
+    m.`sid`,
+    m.`email`,
+    m.`name`,
+    m.`mobile`,
+    m.`birth`,
+    m.`address`,
+    ms.`name` AS `sex`,
+    ml.tier,
+    m.`hero_icon`,
+    mr.role,
+    m.`created_at`,
+    m.`active`
+FROM
+    `member` m
+    JOIN `member_sex` ms ON m.sex_sid = ms.sid
+    JOIN `member_level` ml ON m.`member_level_sid` = ml.sid
+    JOIN `member_role` mr ON m.`role_sid` = mr.sid
+ORDER BY
+    `m`.`created_at` ASC
