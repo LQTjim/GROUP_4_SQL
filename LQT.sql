bba1417534c2a4711dff3f410512e8136350627c
LQTjim <jim6050482@hotmail.com>
2023-05-12 16:13:17 +0800

    update:LQT.sql add 服裝類查表sql

--- LQT.sql
+++ LQT.sql
@@ -1,4 +1,4 @@
---- 會員查表 join sex level role :
+--- 會員查表 join sex level role
 SELECT
     m.`sid`,
     m.`email`,
@@ -11,24 +11,24 @@ SELECT
     m.`hero_icon`,
     mr.role,
     m.`created_at`,
-    CASE
-        m.`active`
-        WHEN 1 THEN '已啟用'
-        WHEN 0 THEN '未啟用'
-        ELSE '未知啟用狀態'
-    END AS `active`
+    m.`active`
+    WHEN 1 THEN '已啟用'
+    WHEN 0 THEN '未啟用'
+    ELSE '未知啟用狀態'
+END AS `active`
 FROM
     `member` m
     JOIN `member_sex` ms ON m.sex_sid = ms.sid
     JOIN `member_level` ml ON m.`member_level_sid` = ml.sid
     JOIN `member_role` mr ON m.`role_sid` = mr.sid
 ORDER BY
-    `m`.`created_at` ASC --健身器材查表
+    `m`.`created_at` ASC;
+
+--健身器材查表
 SELECT
     ed.sid,
     ed.price,
     en.equipment_name,
-    en.description,
     en.description,
     en.category_id,
     ed.stocks,
@@ -40,4 +40,22 @@ FROM
     `equipment_detail` ed
     JOIN `equipment_name` en ON ed.`equipment_id` = en.sid
     JOIN `equipment_color` ec ON ed.color_id = ec.sid
-    JOIN `equipment_size` es ON es.sid = ed.size_id
+    JOIN `equipment_size` es ON es.sid = ed.size_id;
+
+--服裝類查表
+SELECT
+    pd.sid,
+    pd.price,
+    pn.product_name,
+    pn.description,
+    pn.category_id,
+    pd.stocks,
+    pd.picture,
+    pd.created_at,
+    pc.color_name,
+    ps.size
+FROM
+    `product_detail` pd
+    JOIN `product_name` pn ON pd.`product_id` = pn.sid
+    JOIN `product_color` pc ON pd.color_id = pc.sid
+    JOIN `product_size` ps ON ps.sid = pd.size_id;
